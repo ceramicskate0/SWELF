f02d968bec345f4a1d8e838c4189d12f2bbb49a0
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2018-01-19 16:57:36 -0600

    Update install.bat

--- install/install.bat
+++ install/install.bat
@@ -1,3 +1,3 @@
 mkdir 'C:\Program Files (x86)\SWELF'
-powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile iex (New-Object System.Net.WebClient).DownloadFile("https://github.com/ceramicskate0/SWELF/releases/download/0.1.0.4/ConsoleEventLogAutoSearch.exe", "C:\Program Files (x86)\SWELF\SWELF.exe")
+powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile iex (New-Object System.Net.WebClient).DownloadFile("https://github.com/ceramicskate0/SWELF/releases/download/0.1.0.7/ConsoleEventLogAutoSearch.exe", "C:\Program Files (x86)\SWELF\SWELF.exe")
 start 'C:\Program Files (x86)\SWELF\ConsoleEventLogAutoSearch.exe'
