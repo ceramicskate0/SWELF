c9b508abd430bf84fec3721deac39664830afc66
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2018-01-25 01:42:56 -0600

    Update Program.cs

--- ConsoleEventLogAutoSearch/Program.cs
+++ ConsoleEventLogAutoSearch/Program.cs
@@ -1,15 +1,16 @@
 //Written by Ceramicskate0
-//Copyright 2017
+//Copyright 2018
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security.Principal;
 using System.Diagnostics.Eventing.Reader;
 using System.Diagnostics.Eventing;
 using System.Diagnostics;
-using System.IO;
 
-namespace ConsoleEventLogAutoSearch
+
+namespace SWELF
 {
     class Program
     {
@@ -17,85 +18,153 @@ namespace ConsoleEventLogAutoSearch
 
         public static void Main(string[] args)
         {
+            
             try
             {
+                Start_Setup();
+
+                Start_Read_Search_Write_Forward_EventLogs();
+
+                Start_ReadLocal_Logs();
+
+                Start_Send_File_Based_Logs();
+
+                Start_Write_Errors();
+            }
+            catch (Exception e)
+            {
+                Errors.Log_Error("ALERT: SWELF MAIN ERROR: ", e.Message.ToString() + ", Also the app died");
+                Start_Write_Errors();
+                Environment.Exit(2);
+            }
+        }
+
+        private static void Start_Setup()
+        {
+            //CHECK_If_Running_as_Admin();
+            try
+            {
+                Settings.InitializeAppSettings();
+            }
+            catch (Exception e)
+            {
+                Errors.Log_Error("ALERT: SWELF MAIN ERROR: ", "Settings.InitializeAppSettings() " + e.Message.ToString());
+                HostEventLogAgent_Eventlog.WRITE_All_App_EventLog(Settings.CriticalEvents);
+                Start_Write_Errors();
+                Environment.Exit(2);
+            }
+        }
+
+        private static void Start_Run_Plugins()
+        {
+            try
+            {
+                Powershell_Plugin.Run_PS_Script("C:\\Users\\Host\\Downloads\\DeepBlueCLI-master\\DeepBlue.ps1", "C:\\Users\\Host\\Downloads\\DeepBlueCLI-master\\evtx\\psattack-security.evtx");
+                //TODO if found return/forward/add to list of logs to forward
+            }
+            catch (Exception e)
+            {
+                Errors.Log_Error("SWELF PLUGIN ERROR: ", "Powershell_Plugin.Run_PS_Script() " + e.Message.ToString());
+                Network_Forwarder.SEND_Data_from_File("SWELF PLUGIN ERROR: Powershell_Plugin.Run_PS_Script() - " + e.Message.ToString());
+            }
+        }
+
+        private static void Start_Read_Search_Write_Forward_EventLogs()
+        {
+            if (Settings.EventLog_w_PlaceKeeper_List.Count > 0)
+            {
+                for (int x = 0; x < Settings.EventLog_w_PlaceKeeper_List.Count; ++x)//READ and Search
+                {
+                    try
+                    {
+                        EvntLogSearch.READ_EventLog(Settings.EventLog_w_PlaceKeeper_List.ElementAt(x), Settings.EventLog_w_PlaceKeeper[Settings.EventLog_w_PlaceKeeper_List.ElementAt(x)]);
+                        Search_EventLogs search_Obj = new Search_EventLogs(EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI);
+                        EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI = search_Obj.Search(Settings.EventLog_w_PlaceKeeper_List.ElementAt(x));
+                    }
+                    catch (Exception e)
+                    {
+                        Errors.Log_Error("ALERT: SWELF APP ERROR: ", Settings.EventLog_w_PlaceKeeper_List.ElementAt(x) + " " + e.Message.ToString());
+                    }
+                    Start_Write_To_SWELF_EventLogs(x);
+                }
                 try
                 {
-                    Settings.InitializeAppSettings();
+                    Start_Send_EventLogs();
                 }
                 catch (Exception e)
                 {
-                    Errors.Log_Error("SWELF MAIN ERROR: ", "Settings.InitializeAppSettings() "+e.Message.ToString());
-                    HostEventLogAgent_Eventlog.WRITE_All_App_EventLog(Settings.CriticalEvents);
-                    GC.Collect();
-                    Environment.Exit(2);
+                    Errors.Log_Error("SWELF NETWORK ERROR: ", e.Message.ToString());
                 }
-                if (Settings.EventLog_w_PlaceKeeper_List.Count > 0)
-                {
-                    for (int x = 0; x < Settings.EventLog_w_PlaceKeeper_List.Count; ++x)//READ and Search
-                    {
-                        try
-                        {
-                            EvntLogSearch.READ_EventLog(Settings.EventLog_w_PlaceKeeper_List.ElementAt(x), Settings.EventLog_w_PlaceKeeper[Settings.EventLog_w_PlaceKeeper_List.ElementAt(x)]);
-                            Search_EventLogs search_Obj = new Search_EventLogs(EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI);
-                            EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI = search_Obj.Search();
-                        }
-                        catch (Exception e)
-                        {
-                            Errors.Log_Error("SWELF APP ERROR: ", Settings.EventLog_w_PlaceKeeper_List.ElementAt(x)+ " " +e.Message.ToString());
-                        }
+                Settings.UPDATE_EventLog_w_PlaceKeeper_File();
+            }
+        }
 
-                        for (int z = 0; z < EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI.Count; ++z)
-                        {
-                            try
-                            {
-                                HostEventLogAgent_Eventlog.WRITE_EventLog(EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI.ElementAt(z).EventData);
-                            }
-                            catch (Exception e)
-                            {
-                                Errors.Log_Error("SWELF EVENTLOG ERROR: ", Settings.EventLog_w_PlaceKeeper_List.ElementAt(x) + " " + e.Message.ToString());
-                            }
-                        }
-                    }
-                    try
-                    {
+        private static void Start_ReadLocal_Logs()
+        {
+            EvntLogSearch.READ_Local_Log_Files();
+            EvntLogSearch.READ_Local_Log_Dirs();
+        }
 
-                        if (Settings.GET_LogCollector_Location().ToString().Contains("127.0.0.1") == false && String.IsNullOrWhiteSpace(Settings.GET_LogCollector_Location().ToString()) == false)//Does admin want to send off logs?
-                        {
-                            for (int x = 0; x < EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI.Count; ++x)
-                            {
-                                Network_Forwarder.SEND_Eventlogs(EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI.Dequeue());
-                            }
-                        }
-                    }
-                    catch (Exception e)//network resource unavailable. Dont send data and try again next run. No logs will be queued by app only re read
-                    {
-                        Errors.Log_Error("SWELF NETWORK ERROR: ", e.Message.ToString());
-                    }
-                    Settings.UPDATE_EventLog_w_PlaceKeeper_File();
-                }
-                //Read all log files admin wants
-                EvntLogSearch.READ_Local_Log_Files();
-                EvntLogSearch.READ_Local_Log_Dirs();
+        private static void Start_Write_To_SWELF_EventLogs(int x)
+        {
+            for (int z = 0; z < EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI.Count; ++z)
+            {
                 try
                 {
-                    if (Settings.GET_LogCollector_Location().ToString().Contains("127.0.0.1") == false && String.IsNullOrWhiteSpace(Settings.GET_LogCollector_Location().ToString()) == false)//Does admin want to send off logs?
-                    {
-                        for (int z = 0; z < EvntLogSearch.FileContents_From_FileReads.Count; ++z)//send data from local log files to network resource
-                        {
-                            HostEventLogAgent_Eventlog.WRITE_EventLog(EvntLogSearch.FileContents_From_FileReads.ElementAt(z));
-                            Network_Forwarder.SEND_Data_from_File(EvntLogSearch.FileContents_From_FileReads.ElementAt(z));
-                        }
-                    }
+                    HostEventLogAgent_Eventlog.WRITE_EventLog(EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI.ElementAt(z).EventData);
                 }
-                catch (Exception e)//network resource unavailable. Dont send data and try again next run. No logs will be queued by app only re read
+                catch (Exception e)
                 {
-                    Settings.Log_Storage_Location_Unavailable(e.Message.ToString());
+                    Errors.Log_Error("ALERT: SWELF WRITE EVENT LOG ERROR: ", Settings.EventLog_w_PlaceKeeper_List.ElementAt(x) + " HostEventLogAgent_Eventlog.WRITE_EventLog " + e.Message.ToString());
                 }
             }
-            catch (Exception e)//WTF HAPPENED??!?!?!?!
+        }
+
+        private static void Start_Send_EventLogs()
+        {
+            if (Settings.GET_LogCollector_Location().ToString().Contains("127.0.0.1") == false && String.IsNullOrWhiteSpace(Settings.GET_LogCollector_Location().ToString()) == false)//Does admin want to send off logs?
             {
-                Errors.Log_Error("SWELF MAIN ERROR: ", e.Message.ToString() + ", Also the app died");
+                for (int x = 0; x < EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI.Count; ++x)
+                {
+                    Network_Forwarder.SEND_Eventlogs(EvntLogSearch.EventLog_Log_File.EventLogs_From_WindowsAPI.Dequeue());
+                }
+            }
+        }
+
+        private static void Start_Send_File_Based_Logs()
+        {
+            try
+            {
+                if (Settings.GET_LogCollector_Location().ToString().Contains("127.0.0.1") == false && String.IsNullOrWhiteSpace(Settings.GET_LogCollector_Location().ToString()) == false)
+                {
+                    for (int z = 0; z < EvntLogSearch.FileContents_From_FileReads.Count; ++z)
+                    {
+                        HostEventLogAgent_Eventlog.WRITE_EventLog(EvntLogSearch.FileContents_From_FileReads.ElementAt(z));
+                        Network_Forwarder.SEND_Data_from_File(EvntLogSearch.FileContents_From_FileReads.ElementAt(z));
+                    }
+                }
+            }
+            catch (Exception e)//network resource unavailable. Dont send data and try again next run. No logs will be queued by app only re read
+            {
+                Settings.Log_Storage_Location_Unavailable(e.Message.ToString());
+            }
+        }
+
+        private static void Start_Write_Errors()
+        {
+            Errors.WRITE_Errors();
+            Errors.SEND_Errors_To_Central_Location();
+        }
+
+        private static bool CHECK_If_Running_as_Admin()
+        {
+            if (new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
