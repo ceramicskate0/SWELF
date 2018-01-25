65dcadedbf49ea54eb2448b3b66f43151917a3e5
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2018-01-25 01:44:49 -0600

    Update Network_Forwarder.cs

--- ConsoleEventLogAutoSearch/Network_Forwarder.cs
+++ ConsoleEventLogAutoSearch/Network_Forwarder.cs
@@ -1,5 +1,5 @@
 //Written by Ceramicskate0
-//Copyright 2017
+//Copyright 2018
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +9,23 @@ using System.Net;
 using System.Net.Sockets;
 using System.Web;
 
-namespace ConsoleEventLogAutoSearch
+namespace SWELF
 {
     class Network_Forwarder
     {
         private static List<IPAddress> IPAddr = Settings.GET_LogCollector_Location();
-        private static int port = Settings.LogForwardLocation_Port;
+        private static int port = Settings.Log_Forward_Location_Port;
 
         public static void SEND_Eventlogs(EventLogEntry Data)
         {
-            UdpClient udpClient = new UdpClient(11000);
+            UdpClient udpClient = new UdpClient(FIND_Open_SourcePort());
             try
             {
                 for (int x = 0; x < IPAddr.Count; ++x)
                 {
                     udpClient.Connect(IPAddr.ElementAt(x).MapToIPv4().ToString(), port);
-                    Byte[] sendBytes = Encoding.ASCII.GetBytes(GET_Log_OutputFormat(Data));
+                    byte[] sendBytes = Encoding.ASCII.GetBytes(GET_Log_OutputFormat(Data));
                     udpClient.Send(sendBytes, sendBytes.Length);
-                    IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
                     udpClient.Close();
                 }
             }
@@ -36,35 +35,15 @@ namespace ConsoleEventLogAutoSearch
             }
         }
 
-        public static void SEND_Log(string Data)
+        public static void SEND_Data_from_File(string Log_File_Data)
         {
-            UdpClient udpClient = new UdpClient(11000);
+            UdpClient udpClient = new UdpClient(FIND_Open_SourcePort());
             try
             {
                 for (int x = 0; x < IPAddr.Count; ++x)
                 {
                     udpClient.Connect(IPAddr.ElementAt(x).MapToIPv4().ToString(), port);
-                    Byte[] sendBytes = Encoding.ASCII.GetBytes(Data);
-                    udpClient.Send(sendBytes, sendBytes.Length);
-                    IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
-                    udpClient.Close();
-                }
-            }
-            catch (Exception e)
-            {
-                Errors.Log_Error("SWELF SEND_Log ERROR: ", e.Message.ToString());
-            }
-        }
-
-        public static void SEND_Data_from_File(string Data)
-        {
-            UdpClient udpClient = new UdpClient(11001);
-            try
-            {
-                for (int x = 0; x < IPAddr.Count; ++x)
-                {
-                    udpClient.Connect(IPAddr.ElementAt(x).MapToIPv4().ToString(), port);
-                    Byte[] sendBytes = Encoding.ASCII.GetBytes(Data);
+                    byte[] sendBytes = Encoding.ASCII.GetBytes(Log_File_Data);
                     udpClient.Send(sendBytes, sendBytes.Length);
                     IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
                     udpClient.Close();
@@ -79,44 +58,44 @@ namespace ConsoleEventLogAutoSearch
         private static string GET_Log_OutputFormat(EventLogEntry data)
         {
             string format=Settings.Args["outputformat"];
-            string thing;
+            string Data;
             switch (format.ToLower())
             {
                 case  "data":
                     {
-                        thing = data.EventData;
+                        Data = data.EventData;
                         break;
                     }
                 case "syslog":
                     {
-                        thing= DateTime.Now.ToString("MMM dd HH:mm:ss") + " Syslog " +  data.Severity + " " + data.ComputerName + " SWELF   " + data.EventData;
+                        Data = DateTime.Now.ToString("MMM dd HH:mm:ss") + "   " + Settings.ComputerName + "   " + data.Severity + "   " + "SWELF_Syslog" + "   " + data.EventID.ToString() + "   " + data.EventData;
                         break;
 
                     }
                 case "syslogxml":
                     {
-                        thing = DateTime.Now.ToString("MMM dd HH:mm:ss") + " Syslog " + data.Severity + " " + data.ComputerName + " SWELF   " + data.GET_XML_of_Log;
+                        Data = DateTime.Now.ToString("MMM dd HH:mm:ss") + "   " + Settings.ComputerName + "   " + data.Severity + "   " + "SWELF_Syslog" + "   " + data.EventID.ToString() + "   " + data.GET_XML_of_Log;
                         break;
 
                     }
                 case "xml":
                     {
-                        thing = data.GET_XML_of_Log;
+                        Data = data.GET_XML_of_Log;
                         break;
                     }
                 default:
                     {
-                        thing = data.EventData;
+                        Data = data.EventData;
                         break;
                     }
             }
-            return thing;
+            return Data;
         }       
 
         private static int FIND_Open_SourcePort()
         {
             int startingAtPort = 11000;
-            int maxNumberOfPortsToCheck = 500;
+            int maxNumberOfPortsToCheck = 1500;
             var range = Enumerable.Range(startingAtPort, maxNumberOfPortsToCheck);
             var portsInUse =
                 from p in range
