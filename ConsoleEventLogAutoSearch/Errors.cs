269ab1ade8ab0adf088dfb38e9603e7aad85c6c4
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2018-01-25 01:45:29 -0600

    Update Errors.cs

--- ConsoleEventLogAutoSearch/Errors.cs
+++ ConsoleEventLogAutoSearch/Errors.cs
@@ -1,5 +1,5 @@
-﻿//Written by Ceramicskate0
-//Copyright 2017
+//Written by Ceramicskate0
+//Copyright 2018
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,59 +7,79 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace ConsoleEventLogAutoSearch
+namespace SWELF
 {
     class Errors
     {
         private static List<string> ErrorsLog = new List<string>();
         private static string ErrorLogFile = Settings.GET_ErrorLog_Location;
+        private static DriveInfo Disk = new DriveInfo("C");
+        private static long Drives_Available_Space = Disk.AvailableFreeSpace;
 
-        /// <summary>
-        /// Writes errors to console and log to file in batch at end of program
-        /// </summary>
-        /// <param name="CodeInfo"></param>
-        /// <param name="msg"></param>
         public static void Log_Error(string CodeInfo, string msg)
         {
             string err = DateTime.Now + " : " + CodeInfo + " : " + msg + "\n";
             ErrorsLog.Add(err);
-            if (ErrorsLog.Count>100)
+
+            if (ErrorsLog.Count>5)
             {
-                for ( int x = 0; x > ErrorsLog.Count; ++x)
-                {
-                    WriteErrorsToLog(ErrorsLog.ElementAt(x));
-                }
+                WRITE_Errors();
             }
         }
 
-        /// <summary>
-        /// Writes errors to logs ASAP
-        /// </summary>
-        /// <param name="msg"></param>
-        public static void WriteErrorsToLog(string msg)
+        public static void WRITE_Errors_To_Log(string msg)
         {
             if (!File.Exists(ErrorLogFile))
             {
-                using (StreamWriter file = new StreamWriter(ErrorLogFile))
+                using (StreamWriter filewriter = new StreamWriter(ErrorLogFile))
                 {
-                   file.WriteLine(msg);
+                    filewriter.WriteLine(msg);
                 }
+                
             }
             else
             {
                 File.Create(ErrorLogFile).Close();
-                using (StreamWriter file = new StreamWriter(ErrorLogFile))
+                using (StreamWriter filewriter = new StreamWriter(ErrorLogFile))
                 {
-                    file.WriteLine(msg);
+                    filewriter.WriteLine(msg);
                 }
             }
             Settings.ADD_Eventlog_to_CriticalEvents(msg, "SWELF App Error");
             HostEventLogAgent_Eventlog.WRITE_All_App_EventLog(Settings.CriticalEvents);
+
+            CHECK_Error_Log_Size();
         }
 
-        private static void DO_If_Log_File()
+        private static void CHECK_Error_Log_Size()
         {
-            Settings.GET_ErrorLog_Ready();
+            FileInfo Log_App_Log_File = new FileInfo(ErrorLogFile);
+
+            if (Log_App_Log_File.Length > Drives_Available_Space * .0001)
+            {
+                Settings.DELETE_AND_CREATE_File(ErrorLogFile);
+            }
+        }
+
+        public static void WRITE_Errors()
+        {
+            for (int x = 0; x > ErrorsLog.Count; ++x)
+            {
+                WRITE_Errors_To_Log(ErrorsLog.ElementAt(x));
+            }
+            ErrorsLog.Clear();
+        }
+
+        public static void SEND_Errors_To_Central_Location()
+        {
+            string[]  Errors = File.ReadAllLines(ErrorLogFile);
+            if (Settings.GET_LogCollector_Location().ToString().Contains("127.0.0.1") == false && String.IsNullOrWhiteSpace(Settings.GET_LogCollector_Location().ToString()) == false)
+            {
+                for (int x = 0; x > Errors.Length; ++x)
+                {
+                    Network_Forwarder.SEND_Data_from_File(Errors[x]);
+                }
+            }
         }
     }
 }
