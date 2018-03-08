03c21a4703d166c88c834cedaa0fda9206ee2660
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2018-03-08 03:42:42 -0600

    Update AssemblyInfo.cs

--- ConsoleEventLogAutoSearch/Properties/AssemblyInfo.cs
+++ ConsoleEventLogAutoSearch/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleEventLogAutoSearch")]
+[assembly: AssemblyTitle("Simple Windows Event Log Forwarder (SWELF)")]
 [assembly: AssemblyDescription("Simple Windows Event Log Forwarder (SWELF)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CeramicSkate0")]
 [assembly: AssemblyProduct("ConsoleEventLogAutoSearch")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.1")]
+[assembly: AssemblyFileVersion("0.2.0.1")]
