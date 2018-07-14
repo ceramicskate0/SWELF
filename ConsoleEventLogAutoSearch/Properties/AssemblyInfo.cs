610cd8d6d458d9d8452ff286ef3eb4ccafac7993
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2018-07-14 11:49:04 -0400

    0.3.1.0

--- ConsoleEventLogAutoSearch/Properties/AssemblyInfo.cs
+++ ConsoleEventLogAutoSearch/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Simple Windows Event Log Forwarder (SWELF)")]
-[assembly: AssemblyDescription("Simple Windows Event Log Forwarder (SWELF)")]
+[assembly: AssemblyTitle("SWELF")]
+[assembly: AssemblyDescription("Simple Windows Event Log Forwarder")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CeramicSkate0")]
-[assembly: AssemblyProduct("ConsoleEventLogAutoSearch")]
+[assembly: AssemblyCompany("Ceramicksate0")]
+[assembly: AssemblyProduct("SWELF")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("SWELF")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("#####")]
+[assembly: Guid("ebf0ca50-e7f4-4e8e-9c69-95de7e042c65")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.1")]
-[assembly: AssemblyFileVersion("0.2.0.1")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
